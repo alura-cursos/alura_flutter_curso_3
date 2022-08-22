c874aa7ddb58fcbbcac978f0b9ce355ba3e33c3c
Kako <kakomo2@gmail.com>
2022-08-22 16:49:14 -0300

    Aula 2.2

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -4,6 +4,15 @@ import 'package:path/path.dart';
 Future<Database> getDatabase() async {
   final String path = join(await getDatabasesPath(), 'task.db');
   return openDatabase(path, onCreate: (db, version) {
-    db.execute(tabela);
+    db.execute(tableSql);
   }, version: 1,);
 }
+const String tableSql = 'CREATE TABLE $_tablename('
+    '$_name TEXT, '
+    '$_difficulty INTEGER, '
+    '$_image TEXT)';
+
+const String _tablename = 'taskTable';
+const String _name = 'name';
+const String _difficulty = 'difficulty';
+const String _image = 'image';
