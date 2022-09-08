d340788a28ffc63098e28eba71fc4631a0f1ed44
Kako <kakomo2@gmail.com>
2022-09-08 12:31:39 -0300

    Aula 5.3

--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nosso_primeiro_projeto/components/task.dart';
 import 'package:nosso_primeiro_projeto/data/task_dao.dart';
-import 'package:nosso_primeiro_projeto/data/task_inherited.dart';
 import 'package:nosso_primeiro_projeto/screens/form_screen.dart';
 
 class InitialScreen extends StatefulWidget {
@@ -20,32 +19,71 @@ class _InitialScreenState extends State<InitialScreen> {
         title: const Text('Tarefas'),
       ),
       body: Padding(
-        padding: EdgeInsets.only(top: 8, bottom: 70),
-        child: FutureBuilder<List<Task>>(future: TaskDao().findAll(),builder: (context, snapshot){
-          List<Task>? items = snapshot.data;
-          switch(snapshot.connectionState){
+        padding: const EdgeInsets.only(top: 8, bottom: 70),
+        child: FutureBuilder<List<Task>>(
+            future: TaskDao().findAll(),
+            builder: (context, snapshot) {
+              List<Task>? items = snapshot.data;
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return Center(
+                    child: Column(
+                      children: const [
+                        CircularProgressIndicator(),
+                        Text('Carregando'),
+                      ],
+                    ),
+                  );
 
-            case ConnectionState.none:
-              // TODO: Handle this case.
-              break;
-            case ConnectionState.waiting:
-              // TODO: Handle this case.
-              break;
-            case ConnectionState.active:
-              // TODO: Handle this case.
-              break;
-            case ConnectionState.done:
-              if(snapshot.hasData && items != null){
-                if(items.isNotEmpty){
-                  return ListView.builder(itemCount: items.length, itemBuilder: (BuildContext context, int index){
-                    final Task tarefa = items[index];
-                    return tarefa;
-                  });
-                }
+                case ConnectionState.waiting:
+                  return Center(
+                    child: Column(
+                      children: const [
+                        CircularProgressIndicator(),
+                        Text('Carregando'),
+                      ],
+                    ),
+                  );
+                case ConnectionState.active:
+                  return Center(
+                    child: Column(
+                      children: const [
+                        CircularProgressIndicator(),
+                        Text('Carregando'),
+                      ],
+                    ),
+                  );
+                case ConnectionState.done:
+                  if (snapshot.hasData && items != null) {
+                    if (items.isNotEmpty) {
+                      return ListView.builder(
+                          itemCount: items.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            final Task tarefa = items[index];
+                            return tarefa;
+                          });
+                    }
+                    return Center(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          Icons.error_outline,
+                          size: 128,
+                        ),
+                        Text(
+                          'Não há nenhuma Tarefa',
+                          style: TextStyle(fontSize: 32),
+                        ),
+                      ],
+                    ));
+                  }
+                  return const Text('Erro ao carregar tarefas');
               }
-              break;
-          }
-        }),
+              return Text('Erro desconhecido');
+
+            }),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
