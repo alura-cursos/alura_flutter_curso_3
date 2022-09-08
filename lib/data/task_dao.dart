df98ec288180920749ee3f7a0d8d73d0fc5279cf
Kako <kakomo2@gmail.com>
2022-09-08 11:12:35 -0300

    Aula 3.3

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -14,13 +14,32 @@ class TaskDao {
   static const String _image = 'image';
 
   save(Task tarefa) async {}
-  Future<List<Task>> findAll() async{
+
+  Future<List<Task>> findAll() async {
     print('Acessando o findAll: ');
     final Database bancoDeDados = await getDatabase();
-    final List<Map<String,dynamic>> result = await bancoDeDados.query(_tablename);
+    final List<Map<String, dynamic>> result =
+        await bancoDeDados.query(_tablename);
     print('Procurando dados no banco de dados... encontrado: $result');
-    return toList();
+    return toList(result);
   }
-  Future<List<Task>> find(String nomeDaTarefa) async{}
-  delete(String nomeDaTarefa) async{}
+
+  List<Task> toList(List<Map<String, dynamic>> mapaDeTarefas) {
+    print('Convertendo to List:');
+    final List<Task> tarefas = [];
+    for (Map<String, dynamic> linha in mapaDeTarefas) {
+      final Task tarefa = Task(
+        linha[_name],
+        linha[_image],
+        linha[_difficulty],
+      );
+      tarefas.add(tarefa);
+    }
+    print('Lista de Tarefas: ${tarefas.toString()}');
+    return tarefas;
+  }
+
+  Future<List<Task>> find(String nomeDaTarefa) async {}
+
+  delete(String nomeDaTarefa) async {}
 }
