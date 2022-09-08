3a261a7e7dc1a8d4b261120b2b878183ef6b0c2c
Kako <kakomo2@gmail.com>
2022-09-08 11:53:44 -0300

    Aula 4.2

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -17,21 +17,32 @@ class TaskDao {
     print('Iniciando o save: ');
     final Database bancoDeDados = await getDatabase();
     var itemExists = await find(tarefa.nome);
+    Map<String,dynamic> taskMap = toMap(tarefa);
     if (itemExists.isEmpty) {
       print('a Tarefa não Existia.');
-      return await bancoDeDados.insert(_tablename, values);
+      return await bancoDeDados.insert(_tablename, taskMap);
     } else {
       print('a Tarefa existia!');
       return await bancoDeDados.update(
         _tablename,
-        values,
+        taskMap,
         where: '$_name = ?',
         whereArgs: [tarefa.nome],
       );
     }
   }
+  Map<String, dynamic> toMap(Task tarefa) {
+    print('Convertendo to Map: ');
+    final Map<String, dynamic> mapaDeTarefas = Map();
+    mapaDeTarefas[_name] = tarefa.nome;
+    mapaDeTarefas[_difficulty] = tarefa.dificuldade;
+    mapaDeTarefas[_image] = tarefa.foto;
+    print('Mapa de Tarefas: $mapaDeTarefas');
+    return mapaDeTarefas;
+  }
+}
 
-  Future<List<Task>> findAll() async {
+Future<List<Task>> findAll() async {
     print('Acessando o findAll: ');
     final Database bancoDeDados = await getDatabase();
     final List<Map<String, dynamic>> result =
